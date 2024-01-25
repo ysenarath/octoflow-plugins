c570083419af9691a4082b23949294612185f297
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-01-25 16:10:08 -0500

    Add support for reading JSON files

--- src/octoflow_plugins/data/loaders.py
+++ src/octoflow_plugins/data/loaders.py
@@ -1,3 +1,5 @@
+import json
+
 from octoflow.data.loader import dataloader
 
 try:
@@ -5,11 +7,31 @@ try:
 except ImportError:
     pd_read_csv = None
 
+
 __all__ = [
     "read_csv",
 ]
 
 
-@dataloader("csv", extensions=[".csv"], wraps=pd_read_csv, path_arg="filepath_or_buffer")
+@dataloader(
+    "csv",
+    extensions=[".csv"],
+    wraps=pd_read_csv,
+    path_arg="filepath_or_buffer",
+)
 def read_csv(*args, **kwargs):
-    return read_csv(*args, **kwargs)
+    return pd_read_csv(*args, **kwargs)
+
+
+@dataloader(
+    "json",
+    extensions=[".json"],
+    wraps=open,
+    path_arg="file",
+)
+def read_json(*args, **kwargs):
+    with open(*args, **kwargs) as f:
+        data = json.load(f)
+        if isinstance(data, dict):
+            data = [data]
+        return data
